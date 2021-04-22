f8b5b1fdc152bf202d1653b21277098797e5a4d1
Nambiar, Deepak <deepak.nambiar@us.mcd.com>
2021-04-22 21:22:54 +0530

    Setting up environment variables, installing and registering firebase, angularfire and bootstrap 4

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDoXNzDFK8jXX6fHjmGSoxvfTUfYNTgwXA",
+    authDomain: "turbancart-c8f50.firebaseapp.com",
+    databaseURL: "https://turbancart-c8f50-default-rtdb.firebaseio.com",
+    projectId: "turbancart-c8f50",
+    storageBucket: "turbancart-c8f50.appspot.com",
+    messagingSenderId: "113329808349",
+    appId: "1:113329808349:web:fc52bc06b94424cffecc34",
+    measurementId: "G-R6YMP63XGR"
+  }
 };
